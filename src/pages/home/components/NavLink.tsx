cdc208980f5074727ce74f18659cb96e6152e7a7
Carla <carlarhentschel@gmail.com>
2024-09-18 14:25:53 -0300

    feat: create component for navigation

---
+++ src/pages/home/components/NavLink.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+interface INavLinkProps {
+    href?: string;
+    children: React.ReactNode;
+}
+
+export function NavLink({href, children}: INavLinkProps) {
+    const content = <h3 className="text-xs font-normal uppercase cursor-pointer">{children}</h3>
+
+    if (!href) {  
+        return <>{content}</>;  
+    }  
+
+    return (  
+        <Link href={href}>  
+            {content}  
+        </Link>  
+    );  
+   
+}
