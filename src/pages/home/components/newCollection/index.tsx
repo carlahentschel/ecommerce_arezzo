79db42af274febbf16c46fdc266db09f5360f522
Carla <carlarhentschel@gmail.com>
2024-09-24 10:19:28 -0300

    style: style adjusts

--- src/pages/home/components/newCollection/index.tsx
+++ src/pages/home/components/newCollection/index.tsx
@@ -4,24 +4,24 @@ import Livia2 from '/public/assets/Livia2.png';
 
 export function NewCollection() {
   return (
-    <section className="w-full p-8">
-      <div className="flex flex-col md:flex-row md:items-end azul">
-        <div className="flex flex-col laranja">
-          <div className="flex flex-col verde">
-            <h2 className="uppercase lock text-xs font-normal">Nova coleção</h2>
-            <span className="text-2xl font-light tracking-widest">
-              # liviaarezzo
+    <section className="w-full py-10 px-10">
+      <div className="flex flex-col md:flex-row md:items-end">
+        <div className="flex flex-col md:w-4/5">
+          <div className="flex flex-col">
+            <h2 className="uppercase text-xs font-normal">Nova coleção</h2>
+            <span className="uppercase text-2xl font-light tracking-widest">
+              #liviaarezzo
             </span>
           </div>
 
-          <div className="flex items-end verde gap-2 md:-mt-10">
+          <div className="flex items-end md:-mt-10">
             <div>
               <Image
                 src={Livia1}
                 alt="Modelo deitada usando coturno Livia preto com sola rosa"
                 width={400}
                 height={400}
-                className="object-cover md:mt-20"
+                className="object-cover"
               />
             </div>
             <div>
@@ -36,12 +36,12 @@ export function NewCollection() {
           </div>
         </div>
 
-        <div className="flex flex-col verde">
-          <h3 className="text-base font-normal leading-5 mb-3">
+        <div className="flex flex-col md:w-1/5 md:ml-3">
+          <h3 className="text-base font-normal leading-5 mt-2 mb-3">
             <span className="block">Conheça o novo coturno</span>
             <span>Livia Arezzo</span>
           </h3>
-          <button className="text-xs font-normal text-white bg-black rounded-full px-4 py-2">
+          <button className="text-xs font-normal text-white bg-black rounded-full w-24 h-12 px-4 py-2">
             Ver coleção
           </button>
         </div>
