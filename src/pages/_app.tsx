15e030eb7626722d0d8704a860dd9eeab54b8c3d
Carla <carlarhentschel@gmail.com>
2024-09-25 12:03:54 -0300

    feat: create footer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Commissioner } from 'next/font/google';
 import { Header } from './home/components/Header';
+import { Footer } from './home/components/footer';
 
 const comissioner = Commissioner({
   subsets: ['latin'],
@@ -14,6 +15,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <main>
         <Header />
         <Component {...pageProps} />
+        <Footer />
       </main>
     </div>
   );
