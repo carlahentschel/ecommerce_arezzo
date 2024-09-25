78df1c62f8b5a132467e4585f7bfc399314a7f0a
Carla <carlarhentschel@gmail.com>
2024-09-25 11:23:29 -0300

    refactor: adjust formatting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,18 +3,14 @@ import { Header } from './home/components/Header';
 import { PromotionalBanner } from './home/components/PromotionalBanner';
 import { NewCollection } from './home/components/newCollection';
 
-
-
 export default function Home() {
   return (
     <div>
       <main>
         <Header />
         <Banner />
-<PromotionalBanner />
+        <PromotionalBanner />
         <NewCollection />
-
-  
       </main>
       <footer className="w-full bg-slate-400"></footer>
     </div>
