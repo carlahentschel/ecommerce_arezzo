e144b6c344aea19f9e9650ca13769863e19df76f
Carla <carlarhentschel@gmail.com>
2024-09-26 09:26:44 -0300

    feat: starts sandal types component

---
+++ src/pages/sapatos/sandalias/components/TypesSandalsCards.tsx
@@ -0,0 +1,7 @@
+export function TypesSandalsCards() {
+  return (
+    <div className="w-full px-8 py-8 bg-slate-200">
+      <div className="flex flex-col"></div>
+    </div>
+  );
+}
--- src/pages/sapatos/sandalias/index.tsx
+++ src/pages/sapatos/sandalias/index.tsx
@@ -1,9 +1,11 @@
 import { BreadCrumb } from './components/breadCrumb';
+import { TypesSandalsCards } from './components/TypesSandalsCards';
 
 export default function SandalsPDC() {
   return (
     <div>
       <BreadCrumb />
+      <TypesSandalsCards />
     </div>
   );
 }
