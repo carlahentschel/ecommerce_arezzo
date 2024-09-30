84c0b3c6a9c1f8b75e8793fd7903a950cea0b39e
Carla <carlarhentschel@gmail.com>
2024-09-30 17:35:04 -0300

    style: adjusts style

--- src/components/productCard/index.tsx
+++ src/components/productCard/index.tsx
@@ -1,22 +1,21 @@
-import Image from 'next/image';
 import { IProduct } from '@/types/product';
+import Image from 'next/image';
 
 interface IProductCardProps {
   products: IProduct[];
 }
 
 export function ProductCard({ products }: IProductCardProps) {
-  const PRODUCT_ITEMS_LIMIT = 5;
   return (
-    <div className="flex justify-between w-full overflow-x-auto gap-1">
-      {products.slice(0, PRODUCT_ITEMS_LIMIT).map((product, index) => (
-        <div key={index} className="flex flex-col min-w-72 w-full">
-          <div className="relative w-full mb-2 h-96">
+    <div className="grid grid-cols-2 lg:grid-cols-4 w-full gap-1">
+      {products.map((product, index) => (
+        <div key={index} className="flex flex-col w-full">
+          <div className="relative w-full aspect-square">
             <Image
               src={product.images[0].url}
               alt={product.name}
               layout="fill"
-              className="w-full h-full object-cover"
+              className="object-cover"
             />
           </div>
           <div className="flex text-xs px-3 py-3 w-full">
