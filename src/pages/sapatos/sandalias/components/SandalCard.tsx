e237d22117bdb798266956f989b775c232711fa7
Carla <carlarhentschel@gmail.com>
2024-09-30 11:18:45 -0300

    style: style adjustments

--- src/pages/sapatos/sandalias/components/SandalCard.tsx
+++ src/pages/sapatos/sandalias/components/SandalCard.tsx
@@ -10,12 +10,12 @@ export function SandalCard({ products }: ISandalCardProps) {
     <div className="grid grid-cols-4 w-full gap-1">
       {products.map((product, index) => (
         <div key={index} className="flex flex-col w-full">
-          <div className="relative w-full bg-slate-400 h-96">
+          <div className="relative w-full aspect-square">
             <Image
               src={product.images[0].url}
               alt={product.name}
               layout="fill"
-              className="w-full h-full object-cover"
+              className="object-cover"
             />
           </div>
           <div className="flex text-xs px-3 py-3 w-full">
--- src/pages/sapatos/sandalias/components/sandalsList/index.tsx
+++ src/pages/sapatos/sandalias/components/sandalsList/index.tsx
@@ -8,7 +8,7 @@ export function SandalsList({
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <section className="w-full py-5">
-      <div className="flex flex-col-reverse items-start lg:flex-row lg:items-center px-6 w-full">
+      <div className="flex flex-col-reverse items-start lg:flex-row lg:items-center px-6 py-3 lg:py-5 w-full">
         <button className="flex justify-center items-center gap-3 text-sm border rounded-full min-w-28 h-10 mr-9">
           <FilterIcon />
           Filtrar
