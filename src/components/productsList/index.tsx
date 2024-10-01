b77f5e00ff23c7a93548fbb2485b1564a515ea07
Carla <carlarhentschel@gmail.com>
2024-10-01 10:24:46 -0300

    style: remove padding from section

--- src/components/productsList/index.tsx
+++ src/components/productsList/index.tsx
@@ -7,7 +7,8 @@ export function ProductsList({
   products,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
-    <section className="w-full py-5">
+    <section className="w-full">
+      <hr />
       <div className="flex flex-col-reverse items-start lg:flex-row lg:items-center px-6 py-3 lg:py-5 w-full">
         <button className="flex justify-center items-center gap-3 text-sm border rounded-full min-w-28 h-10 mr-9">
           <FilterIcon />
