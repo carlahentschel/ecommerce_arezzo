65b256bdd2e3ec764fb5fc71e54e51f7b63425de
Carla <carlarhentschel@gmail.com>
2024-10-01 10:25:51 -0300

    feat: add icon buttons to the card

--- src/components/productCard/index.tsx
+++ src/components/productCard/index.tsx
@@ -1,5 +1,7 @@
 import { IProduct } from '@/types/product';
 import Image from 'next/image';
+import { FavoriteIcon } from '../icons/FavoriteIcon';
+import { BagIcon } from '../icons/BagIcon';
 
 interface IProductCardProps {
   products: IProduct[];
@@ -10,13 +12,21 @@ export function ProductCard({ products }: IProductCardProps) {
     <div className="grid grid-cols-2 lg:grid-cols-4 w-full gap-1">
       {products.map((product, index) => (
         <div key={index} className="flex flex-col w-full">
-          <div className="relative w-full aspect-square">
+          <div className="relative w-full flex flex-col items-end py-4 px-4 aspect-square">
             <Image
               src={product.images[0].url}
               alt={product.name}
               layout="fill"
               className="object-cover"
             />
+            <div className="absolute flex gap-4">
+              <button>
+                <BagIcon />
+              </button>
+              <button>
+                <FavoriteIcon />
+              </button>
+            </div>
           </div>
           <div className="flex text-xs px-3 py-3 w-full">
             <h3 className="truncate font-normal flex-1">{product.name}</h3>
